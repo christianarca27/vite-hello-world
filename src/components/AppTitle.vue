cf5c44b6a9ea368cfac4b186b26679743db89ee4
christianarca27 <christian.arca.mail@gmail.com>
2023-03-23 14:48:17 +0100

    feat: creato component AppTitle e inserito in pagina

---
+++ src/components/AppTitle.vue
@@ -0,0 +1,15 @@
+<script>
+export default {
+    data() {
+        return {
+            title: "Vue Hello World",
+        }
+    },
+}
+</script>
+
+<template>
+    <h1>{{ title }}</h1>
+</template>
+
+<style scoped></style>
