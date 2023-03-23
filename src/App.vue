ce145d8090fd63b26097dc57be7bd323f9948620
christianarca27 <christian.arca.mail@gmail.com>
2023-03-23 15:02:14 +0100

    feat: bonus, creato componente AppHero e inserito in pagina

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,19 @@
 <script>
 import AppTitle from "./components/AppTitle.vue";
+import AppHero from "./components/AppHero.vue";
 
 export default {
   components: {
     AppTitle,
+    AppHero,
   }
 }
 </script>
 
 <template>
   <AppTitle></AppTitle>
+
+  <AppHero></AppHero>
 </template>
 
 <style scoped></style>
---
+++ src/components/AppHero.vue
@@ -0,0 +1,21 @@
+<script>
+export default {
+    data() {
+        return {
+            title: "Vue Hello World",
+        }
+    },
+}
+</script>
+
+<template>
+    <div id="hero-wrapper">
+        <img src="/images/hero.jpg" alt="Hero image">
+    </div>
+</template>
+
+<style scoped>
+#hero-wrapper {
+    text-align: center;
+}
+</style>
