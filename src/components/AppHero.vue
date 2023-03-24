c24f4076e715791695e71782bdb757a9f31bfdd8
christianarca27 <christian.arca.mail@gmail.com>
2023-03-24 10:15:04 +0100

    feat: migliorato AppHero

--- src/components/AppHero.vue
+++ src/components/AppHero.vue
@@ -9,13 +9,39 @@ export default {
 </script>
 
 <template>
-    <div id="hero-wrapper">
-        <img src="/images/hero.jpg" alt="Hero image">
+    <div id="hero">
+        <h2>HERO</h2>
     </div>
 </template>
 
 <style scoped>
-#hero-wrapper {
-    text-align: center;
+#hero {
+    background-image: url(/images/hero.jpg);
+    height: 400px;
+
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    position: relative;
+}
+
+#hero::before {
+    background-color: black;
+    width: 100%;
+    height: 100%;
+
+    position: absolute;
+    top: 0;
+    left: 0;
+
+    content: "";
+    opacity: .6;
+}
+
+#hero h2 {
+    font-size: 40px;
+
+    z-index: 1;
 }
 </style>
